41562ca48d78dff5bc764d0c4ab45015bd2e9eb7
Nick S <mycytasorokin@gmail.com>
2020-01-28 21:40:50 +0200

    js

--- passw/script.js
+++ passw/script.js
@@ -4,7 +4,7 @@ var button = document.querySelector('.btn'),
     
     button.onclick = function arrayCheck() {
         var myList = textarea.value.split('\n');
-    for (var i = 0, ln = myList.length; i < num+1; ++i) {
+    for (var i = 0, num = document.getElementById('num').value; i < num; i++) {
         if (myList[i].match(/[a-z]/) && myList[i].match(/[A-Z]/) && myList[i].match(/[0-9]/) && myList[i].length >=10) {
             myList[i] = myList[i] + ' is strong';
             } else {
